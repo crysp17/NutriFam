23d1b5955c51b9aa4b0da3ce31415a2ebecf9e1c
DavidBocek <ddperera@mit.edu>
2016-04-13 17:49:18 -0400

    getting updated files

--- calendar.js
+++ calendar.js
@@ -1,7 +1,69 @@
 $(function() {
-    var meals = {breakfast: [[{meal: 'Oatmeal', members: ['Joanne', 'Jimbob']},{meal:'Cheerios', members:['Mark', 'Matthew']}],[{meal: 'Oatmeal', members: ['Joanne', 'Jimbob']},{meal:'Cheerios', members:['Mark', 'Matthew']}],[{meal: 'Oatmeal', members: ['Joanne', 'Jimbob']},{meal:'Cheerios', members:['Mark', 'Matthew']}],[{meal: 'Oatmeal', members: ['Joanne', 'Jimbob']},{meal:'Cheerios', members:['Mark', 'Matthew']}],[{meal: 'Oatmeal', members: ['Joanne', 'Jimbob']},{meal:'Cheerios', members:['Mark', 'Matthew']}],[{meal: 'Oatmeal', members: ['Joanne', 'Jimbob']},{meal:'Cheerios', members:['Mark', 'Matthew']}],[{meal: 'Oatmeal', members: ['Joanne', 'Jimbob']},{meal:'Cheerios', members:['Mark', 'Matthew']}]],
-        lunch: [[{meal: 'Beef Stew', members: ['Joanne', 'Jimbob','Mark', 'Matthew']}],[{meal: 'Chicken Breast', members: ['Joanne', 'Jimbob']},{meal:'Ham & Brie Sandwich', members:['Mark', 'Matthew']}],[{meal: 'Quesadilla', members: ['Joanne', 'Jimbob']},{meal:'Tuna Salad Sandwich', members:['Mark', 'Matthew']}],[{meal: 'Chicken Breast', members: ['Joanne', 'Jimbob']},{meal:'PB & Banana Sandwich', members:['Mark', 'Matthew']}],[{meal: 'Wedge Salad', members: ['Joanne', 'Jimbob']},{meal:'Chicken Salad', members:['Mark', 'Matthew']}],[{meal: 'Chef Salad', members: ['Joanne', 'Jimbob']},{meal:'Turkey $ Swiss', members:['Mark', 'Matthew']}],[{meal: 'Fried Chicken', members: ['Joanne', 'Jimbob','Mark', 'Matthew']}]],
-        dinner: [[{meal: 'Spaghetti', members: ['Joanne', 'Jimbob','Mark', 'Matthew']}],[{meal: "Shepherd's Pie", members: ['Joanne', 'Jimbob','Mark', 'Matthew']}],[{meal: 'Chicken Alfredo', members: ['Joanne', 'Jimbob','Mark', 'Matthew']}],[{meal: 'Pork Chops', members: ['Joanne', 'Jimbob','Mark', 'Matthew']}],[],[{meal: 'Fried Rice', members: ['Joanne', 'Jimbob','Mark', 'Matthew']}],[{meal: 'Tacos', members: ['Joanne', 'Jimbob','Mark', 'Matthew']}]]}
+    var meals = 
+        {
+            breakfast: 
+            [
+            [{meal: 'Oatmeal', members: ['Joanne', 'Jimbob']},
+              {meal:'Cheerios', members:['Mark', 'Matthew']}],
+
+             [{meal: 'Oatmeal', members: ['Joanne', 'Jimbob']},
+              {meal:'Cheerios', members:['Mark', 'Matthew']}],
+
+             [{meal: 'Oatmeal', members: ['Joanne', 'Jimbob']},
+              {meal:'Cheerios', members:['Mark', 'Matthew']}],
+
+             [{meal: 'Oatmeal', members: ['Joanne', 'Jimbob']},
+              {meal:'Cheerios', members:['Mark', 'Matthew']}],
+
+             [{meal: 'Oatmeal', members: ['Joanne', 'Jimbob']},
+              {meal:'Cheerios', members:['Mark', 'Matthew']}],
+
+             [{meal: 'Oatmeal', members: ['Joanne', 'Jimbob']},
+              {meal:'Cheerios', members:['Mark', 'Matthew']}],
+
+             [{meal: 'Oatmeal', members: ['Joanne', 'Jimbob']},
+              {meal:'Cheerios', members:['Mark', 'Matthew']}]
+            ],
+
+            lunch: 
+            [
+            [{meal: 'Beef Stew', members: ['Joanne', 'Jimbob','Mark', 'Matthew']}],
+
+            [{meal: 'Chicken Breast', members: ['Joanne', 'Jimbob']},
+             {meal:'Ham & Brie Sandwich', members:['Mark', 'Matthew']}],
+
+            [{meal: 'Quesadilla', members: ['Joanne', 'Jimbob']},
+             {meal:'Tuna Salad Sandwich', members:['Mark', 'Matthew']}],
+
+            [{meal: 'Chicken Breast', members: ['Joanne', 'Jimbob']},
+             {meal:'PB & Banana Sandwich', members:['Mark', 'Matthew']}],
+
+            [{meal: 'Wedge Salad', members: ['Joanne', 'Jimbob']},
+             {meal:'Chicken Salad', members:['Mark', 'Matthew']}],
+
+            [{meal: 'Chef Salad', members: ['Joanne', 'Jimbob']},
+             {meal:'Turkey $ Swiss', members:['Mark', 'Matthew']}],
+
+            [{meal: 'Fried Chicken', members: ['Joanne', 'Jimbob','Mark', 'Matthew']}]
+            ],
+
+            dinner: 
+            [
+            [{meal: 'Spaghetti', members: ['Joanne', 'Jimbob','Mark', 'Matthew']}],
+
+            [{meal: "Shepherd's Pie", members: ['Joanne', 'Jimbob','Mark', 'Matthew']}],
+
+            [{meal: 'Chicken Alfredo', members: ['Joanne', 'Jimbob','Mark', 'Matthew']}],
+
+            [{meal: 'Pork Chops', members: ['Joanne', 'Jimbob','Mark', 'Matthew']}],
+
+            [],
+
+            [{meal: 'Fried Rice', members: ['Joanne', 'Jimbob','Mark', 'Matthew']}],
+
+            [{meal: 'Tacos', members: ['Joanne', 'Jimbob','Mark', 'Matthew']}]
+            ]
+        }
     var nickname = {Joanne: 'Jo', Jimbob: 'Ji', Matthew: 'Mt', Mark: 'Mr'};
     var keys = Object.keys(meals);
     for (var i = 0; i < keys.length; i++){
@@ -23,4 +85,6 @@ $(function() {
         }
 
     }
+
+
 })
