d95d717267187e24bd3e2c49d5921636e6e3cf0b
crysp <crysp@mit.edu>
2016-05-02 00:37:02 -0400

    inner fam out

--- NutriFam/main.js
+++
@@ -1,6 +0,0 @@
-$(function() {
-    var famday = {Joanne:[{label: 'Fat', value: 395},{label: 'Protein', value:376},{label: 'Carbs', value:681}],
-        Jimbob:[{label: 'Fat', value: 530},{label: 'Protein', value: 300},{label: 'Carbs', value: 920}],
-        Matthew:[{label: 'Fat', value: 507},{label: 'Protein', value: 320},{label: 'Carbs', value: 1052}],
-        Mark:[{label: 'Fat', value: 525},{label: 'Protein', value: 285},{label: 'Carbs', value: 933}],}
-})
